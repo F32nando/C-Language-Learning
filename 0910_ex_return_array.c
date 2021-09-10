31a322e9c5b9740da90d538cf0abf49c1251f8e1
F32nando <90126457+F32nando@users.noreply.github.com>
2021-09-10 16:58:38 +0800

    Add files via upload

--- 0910_ex_return_array.c
+++ 0910_ex_return_array.c
@@ -25,10 +25,11 @@ fun1回傳 五個元素 然後再送至show函數輸出 五個元素
 int* fun1();
 void show(int *kp);
 
+int size; //元素量
 
 int main()
 {
-    int size=5;
+    //int size=5;
     //int *k = (int*)malloc(size*sizeof(int));
     int *k;
     k = fun1();
@@ -39,14 +40,17 @@ int main()
 
 int* fun1()
 {
-    int size=5;
-    int *p = (int*)malloc(size*sizeof(int));
     
-    p[0]=12;
-    p[1]=13;
-    p[2]=15;
-    p[3]=19;
-    p[4]=23;
+    int t[] = {10,20,30,40,50,60};
+    size= sizeof(t)/sizeof(int);
+    
+    int *p = (int*)malloc(size*sizeof(int));
+
+    int i;
+    for(i=0;i<size;i++)
+    {
+        p[i] = t[i];
+    }
 
     //這邊是用來檢查到底有沒有東西
     //int i=0;
@@ -62,8 +66,9 @@ int* fun1()
 void show(int *kp)
 {
     printf("show印出的\n");
-    int i=0;
-    for(i=0;i<sizeof(kp);i++)
+    int i;
+
+    for(i=0;i<size;i++)
     {
         printf("%d\n",kp[i]);
     }
